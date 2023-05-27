2d04cc1a59dddd763886cd9eae4e36c71dfcd88a
Karen Russeth <kvrusseth@gmail.com>
2023-05-27 10:14:28 -0400

    add jasmine standalone

---
+++ tip-pool/jasmine-standalone-5.0.0/spec/servers.test.js
@@ -0,0 +1,39 @@
+describe('Servers test with setup and tear-down', function() {
+     beforeEach(function () {
+      serverNameInput.value = 'Alice';
+     });
+   
+     it('should add a new server to allServers on submitServerInfo()', function () {
+      submitServerInfo();
+
+      expect(Object.keys(allServers).length).toEqual(1);
+      expect(allServers['server' + serverId].serverName).toEqual('Alice');
+     });
+
+     it('should not add a new server on submitServerInfo() with empty input', function () {
+      serverNameInput.value = '';
+      submitServerInfo();
+  
+      expect(Object.keys(allServers).length).toEqual(0);
+    });
+
+     it("should add another server to the server table on updateServerTable()", function () {
+      serverId += 1;
+      allServers["server" + serverId.toString()] = {
+        serverName: serverNameInput.value,
+      };
+      updateServerTable();
+      const row = document.getElementById("server" + serverId.toString());
+      expect(row.children[0].innerText).toEqual("Alice");
+    });
+   
+     afterEach(function () {
+       // teardown logic
+       allServers = {};
+       serverId = 0;
+       serverTbody.innerHTML = '';
+     });
+})
+
+
+
